2e40d8f1935940577584d4053902c80ebec632eb
dc-choi <cheek0805@naver.com>
2022-08-23 21:32:27 +0900

    use SessionStore with Promise

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 var RedisStore = require("connect-redis")(session);
 var { createClient } = require("redis");
 var crypto = require("crypto");
@@ -29,18 +30,46 @@ app.use(
     secret: "keyboard cat",
     resave: false,
   })
-)
+);
 
-app.use('/', (req, res, next) => {
+// var options ={
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'rhdwl9685*',
+//     database: 'idp'
+// };
+// var sessionStore = new MySQLStore(options);
+
+// app.use(session({
+//   secret:"asdfasffdas",
+//   resave:false,
+//   saveUninitialized:true,
+//   store: sessionStore
+// }));
+
+app.use('/', async (req, res, next) => {
 	var random = crypto.randomBytes(32).toString('base64');
 	req.session.userId = random;
 	req.session.save();
-	redisClient.get('sess:' + req.sessionID, (err, session) => {
-		console.log('GET Data In Redis', session);
-	})
-	console.log(req.session);
-	console.log(req.session.id);
-	console.log(req.sessionID);
+	console.log(1);
+	const getSession = new Promise(function (resolve, reject) {
+		// do something async here..
+		req.sessionStore.get(req.sessionID, (error, session) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(session);
+			}
+		});
+	});
+	await getSession.then((session) => {
+		console.log(session);
+	});
+	console.log(3);
+	// console.log(req.session);
+	// console.log(req.session.id);
+	// console.log(req.sessionID);
 	// req.session.destroy(() => {
 		// 	console.log('session done');
 	// })
